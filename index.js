9deea59ea97aa6b906d202cc4e8f13944a265d34
RafiNazhmiNugraha <rafinazhminugraha@upi.edu>
2025-03-09 22:45:56 +0700

    add the structure

---
+++ index.js
@@ -0,0 +1,14 @@
+function generatePassword(lenght, includeUppercase, includeLowercase, includeNumbers, includeSymbols) {
+  
+  return ``;
+}
+
+const passwordLenght = 10;
+const includeUppercase = true;
+const includeLowercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLenght, includeUppercase, includeLowercase, includeNumbers, includeSymbols);
+
+console.log(`Your Password : ${password}`);
